3721bfcfcd37d764947b3af8d7afe7dda3f690fd
Paramhans <paramhanspal65@gmail.com>
2025-01-27 22:45:59 +0530

    tweets all

--- src/routes/tweet.router.js
+++ src/routes/tweet.router.js
@@ -1,17 +1,19 @@
-import { Router } from 'express';
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
-    createTweet,
-    deleteTweet,
-    getUserTweets,
-    updateTweet,
-} from "../controllers/tweet.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+  createTweet,
+  deleteTweet,
+  updateTweet,
+  getUserTweets,
+} from "../controllers/tweet.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.use(verifyJWT, upload.none()); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/").post(createTweet);
 router.route("/user/:userId").get(getUserTweets);
 router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 
-export default router
+export default router;
